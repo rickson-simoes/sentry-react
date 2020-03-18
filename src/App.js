0bd908490de06bc1c11f793b72ea3c18a15f6bdf
Rickson <rickson.simoes@hotmail.com>
2020-03-18 15:49:01 -0300

    :sparkles: Primeiro Commit - Sem o sentry

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    valor : ''
+  }
+
+  digitarTexto = e  => {
+    this.setState({valor: e.target.value });
+    console.log(this.state.valor);
+  }
+
+  enviarErro = e => {
+    e.preventDefault();
+    const { valor } = this.state;
+
+    if (valor) {
+      throw new Error(`Erro - Teste: ${valor}`);
+    }
+  }
+
+  render () {
+    return (
+      <>   
+        <h3>Nome do erro: {this.state.valor}</h3>
+
+        <form onSubmit={this.enviarErro}>
+          <input onChange={this.digitarTexto} type="text" name="name" />
+          <input type="submit" value="Enviar" name="name" />
+        </form>
+        
+      </>
+    )
+  }
+
 }
 
 export default App;
