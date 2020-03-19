b8870e3d1e07aa2c592711765e661636bda33c5c
Rickson <rickson.simoes@hotmail.com>
2020-03-19 18:15:45 -0300

    :pencil: :wrench: Configuracoes Finais para executar

--- sentryUploadSourcemaps.js
+++ sentryUploadSourcemaps.js
@@ -4,10 +4,10 @@ const { upload } = require('sentry-files');
 require('dotenv/config');
 
 upload({
-  version: process.env.REACT_APP_SENTRY_PROJECT_RELEASE,
-  organization: process.env.REACT_APP_SENTRY_ORG,
-  project: process.env.REACT_APP_SENTRY_PROJECT,
-  token: process.env.REACT_APP_SENTRY_API_TOKEN,
+  version:process.env.REACT_APP_SENTRY_PROJECT_RELEASE,
+  organization:process.env.REACT_APP_SENTRY_ORG,
+  project:process.env.REACT_APP_SENTRY_PROJECT,
+  token:process.env.REACT_APP_SENTRY_API_TOKEN,
   files: getFiles(),
 })
   .then(data => console.log('----- SUCCESS ----\n', data))
@@ -19,10 +19,21 @@ upload({
     const assetsFile = path.resolve(BUILD_DIR, 'asset-manifest.json');
     const filePaths = require(assetsFile);
 
+/*
+ * Pela variavel filePaths, irá ser feita a busca dentro do ./build/asset-manifest.json
+ * Como o asset-manifest.json possui 2 objetos, foi necessario definir qual era para ser feito o map
+ * Logo é preciso destinguir o objeto por filePaths.files
+ */
     const filePathsFiles = filePaths.files;
 
+/*
+ * path.resolve() não está funcionando dentro do map, foi preciso definir um caminho.
+ */
     const Paths = path.resolve('build');
 
+/*
+ * Faz a leitura de todos os arquivos e retorna os valores para o files: getFiles() ;
+ */
     const jsFilesRegex = /(\.js(.map)?)$/;
     return Object.keys(filePathsFiles)
       .filter(f => jsFilesRegex.test(f))
